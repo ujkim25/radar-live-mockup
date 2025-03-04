24d862a445cbaef3bd1b9a05529cfc4c28b39257
ujkim25 <ujkim25@gmail.com>
2025-03-04 09:32:24 +0900

    change port number to 4001

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors();
-  await app.listen(process.env.PORT ?? 4000);
+  await app.listen(process.env.PORT ?? 4001);
 }
 bootstrap();
